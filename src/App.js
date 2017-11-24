5c8b12e24ee5ce89d56a466611df06391b713836
villehyv <Fouler90@gmail.com>
2017-11-24 13:40:01 +0200

    Background
    Background color changed and grind in place

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
+import Leftscreen from './components/Leftscreen'
+import Rightscreen from './components/Rightscreen'
+import { Grid, Row, Col} from 'react-bootstrap'
+import { render} from 'react-dom'
+import 'bootstrap/dist/css/bootstrap.min.css'
+//import 'bootstrap/dist/react-bootstrap.min.css'
 class App extends Component {
   render() {
     return (
-      <div>
-        Project ForestIndicator
-        Test commit
-      </div>
+      App = (
+        <div>
+          <Grid>
+            <Row className="show-grid">
+            <Col xs={6} md={4}> <Leftscreen /></Col>
+            <Col xs={12} md={8}> <Rightscreen /></Col>
+            </Row>
+          </Grid>
+          </div>
+        
+      )
     );
   }
 }
-
 export default App;
---
+++ src/components/Rightscreen.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Rightscreen.css'
+
+class Rightscreen extends Component {
+    render () {
+        return (
+            <div className= 'content'>
+                RightScreenn
+            </div>
+        )
+    }
+}
+
+export default Rightscreen
