0c7b874f497459e5675df0fb5d9e939cdbfef209
villehyv <Fouler90@gmail.com>
2017-11-28 12:30:50 +0200

    ScrollBar minor update
    Updated scrollbar to 'AutoHeight'

--- src/components/Scrollbar.js
+++ src/components/Scrollbar.js
@@ -4,10 +4,14 @@ import { Scrollbars } from 'react-custom-scrollbars';
 class Scrollbar extends Component {
     
     render () {
-        var width2 =Element.offsetWidth;
-        var height2 = Element.offsetHeight;
+        var width2 =Element.offsetWidth; // https://www.w3schools.com/jsref/prop_element_offsetwidth.asp Its getting the width of an element in pixels
+       /* var height2 = Element.offsetHeight*/
         return (
-            <Scrollbars style={{ width: width2, height: 625 }}
+            <Scrollbars
+            autoHeight
+            autoHeightMin={50}
+            autoHeightMax={800}
+            style={{ width: width2 }}
                             >
             <p>Some great content...</p>
             <br/>
