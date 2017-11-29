c05abf2e8706f38ad58b7c25c81cff2a8dccd1a0
villehyv <Fouler90@gmail.com>
2017-11-29 08:46:42 +0200

    Scrollbar Minor changes

--- src/components/Scrollbar.js
+++ src/components/Scrollbar.js
@@ -5,13 +5,13 @@ class Scrollbar extends Component {
     
     render () {
         var width2 =Element.offsetWidth; // https://www.w3schools.com/jsref/prop_element_offsetwidth.asp Its getting the width of an element in pixels
-       /* var height2 = Element.offsetHeight*/
+        
         return (
             <Scrollbars
             autoHeight
-            autoHeightMin={50}
-            autoHeightMax={800}
-            style={{ width: width2 }}
+            autoHeightMin={500}
+            autoHeightMax={1200}
+            style={{ width: width2  }}
                             >
             <p>Some great content...</p>
             <br/>
