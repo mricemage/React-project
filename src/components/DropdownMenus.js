a629ab7a768c5295dbafffedf0ab871a0b45a5b9
ThangHo <t5homi00@students.oamk.fi>
2017-12-01 10:39:32 +0200

    First try again
    Testing new method!

--- src/components/DropdownMenus.js
+++ src/components/DropdownMenus.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { UncontrolledButtonDropdown, Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
-
+import LocalizedStrings from 'react-localization'
 
 class DropdownMenus extends Component {
     constructor(props){
@@ -10,6 +10,7 @@ class DropdownMenus extends Component {
         this.state = {
         dropdownOpen: false
         };
+    
 }
 
 toggle() {
@@ -19,10 +20,39 @@ toggle() {
   }
 
 
-    render () {
+
+ render () {
+    let strings = new LocalizedStrings ({
+        en: {
+            ScenarioSelection:"Scenario Collection",
+            ForestryCenters:"Forestry Centers",
+            Area:"Area",
+            ScenarioCollection:"Scenario Collection",
+            Time:"Time",
+            IndicatorSelection:"Indicator Selection",
+            ProductCollections:"Product Collections",
+            Diversity:"Diversity",
+            Carbon:"Carbon",
+            Other:"Other"
+        },
+        fi: {
+            ScenarioSelection:"Skenarrioiden valinta",
+            ForestryCenters:"Aluetaso",
+            Area:"Alue",
+            ScenarioCollection:"Skenaariokokoelma",
+            Time:"Ajankohta",
+            IndicatorSelection:"Indikaattoreiden valinta",
+            ProductCollections:"Keruutuotteet",
+            Diversity:"Monimuotoisuus",
+            Carbon:"Hiili",
+            Other:"Muut"
+        }
+    });
+
+
         return (
            <div>
-               <label> Regional Level </label>
+               <label> {strings.ScenarioSelection} </label>
         <Dropdown isOpen={this.state.dropdownOpen} toggle={this.toggle}>
                 <DropdownToggle caret>
                     Region Level
@@ -64,5 +94,7 @@ toggle() {
         )
     }
 }
+
+
 
 export default DropdownMenus
