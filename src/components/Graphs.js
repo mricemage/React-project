96dcb9f408df85b729a5341206d401c14f8db9b8
Luitzen-Jan <l.j.o.beiboer@st.hanze.nl>
2017-12-15 09:01:03 +0200

    Buttons

--- src/components/Graphs.js
+++ src/components/Graphs.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react'
+import {Table} from 'reactstrap';
 const ReactHighCharts = require('react-highcharts')
 
 class Graphs extends Component {
-
+    constructor(props){
+        super(props);
+       
+      }
     
 
     render () {
+
 
         const config = {chart: {
             plotBackgroundColor: null,
@@ -42,8 +47,7 @@ class Graphs extends Component {
             }, {
                 name: 'Laiteläheinen',
                 y: 24.03,
-                sliced: true,
-                selected: true
+                
             }, {
                 name: 'Projektit',
                 y: 10.38
@@ -63,7 +67,47 @@ class Graphs extends Component {
         return (
             
             <div className="graph" id="graph">
+            <table id="table">
+            <tr>
+              <th>Company</th>
+              <th>Contact</th>
+              <th>Country</th>
+            </tr>
+            <tr>
+              <td>Alfreds Futterkiste</td>
+              <td>Maria Anders</td>
+              <td>Germany</td>
+            </tr>
+            <tr>
+              <td>Centro comercial Moctezuma</td>
+              <td>Francisco Chang</td>
+              <td>Mexico</td>
+            </tr>
+            <tr>
+              <td>Ernst Handel</td>
+              <td>Roland Mendel</td>
+              <td>Austria</td>
+            </tr>
+            <tr>
+              <td>Island Trading</td>
+              <td>Helen Bennett</td>
+              <td>UK</td>
+            </tr>
+            <tr>
+              <td>Laughing Bacchus Winecellars</td>
+              <td>Yoshi Tannamuri</td>
+              <td>Canada</td>
+            </tr>
+            <tr>
+              <td>Magazzini Alimentari Riuniti</td>
+              <td>Giovanni Rovelli</td>
+              <td>Italy</td>
+            </tr>
+          </table>
                 <ReactHighCharts config = {config}></ReactHighCharts>
+                
+                  
+               
             </div>
 
         )
